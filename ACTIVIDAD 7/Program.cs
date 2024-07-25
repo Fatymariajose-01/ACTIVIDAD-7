7927ca38a4d84f4d29b0c267181736cd1355011d
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-24 18:07:59 -0600

    Puse el menú

--- ACTIVIDAD 7/Program.cs
+++ ACTIVIDAD 7/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿static void MostrarMenu()
+{
+    Console.WriteLine("Cálculos estadísticos básicos");
+    Console.WriteLine("1. Pedir (n) números");
+    Console.WriteLine("2. Calcular y Mostrar Media");
+    Console.WriteLine("3. Calcular y Mostrar Mediana");
+    Console.WriteLine("4. Calcular y Mostrar Moda");
+    Console.WriteLine("5. Calcular y Mostrar Desviación Estandar");
+    Console.WriteLine("6. Volver a pedir números");
+}
+
