f665492bd814965c0ac476867b9a0c5ce1f20a24
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-24 18:55:04 -0600

    Arreglé y gregué switch y puse los nombre que le pondré a mis métodos

--- ACTIVIDAD 7/Program.cs
+++ ACTIVIDAD 7/Program.cs
@@ -1,11 +1,47 @@
-﻿static void MostrarMenu()
+﻿static void EnseñarMenu()
 {
-    Console.WriteLine("Cálculos estadísticos básicos");
-    Console.WriteLine("1. Pedir (n) números");
-    Console.WriteLine("2. Calcular y Mostrar Media");
-    Console.WriteLine("3. Calcular y Mostrar Mediana");
-    Console.WriteLine("4. Calcular y Mostrar Moda");
-    Console.WriteLine("5. Calcular y Mostrar Desviación Estandar");
-    Console.WriteLine("6. Volver a pedir números");
+    MostrarMenu();
 }
 
+// Método para mostrar el menú
+static void MostrarMenu()
+{
+    Console.WriteLine("Calculadora de estadísticas básicas");
+    Console.WriteLine("1. Ingresar números");
+    Console.WriteLine("2. Calcular y mostrar media");
+    Console.WriteLine("3. Calcular y mostrar mediana");
+    Console.WriteLine("4. Calcular y mostrar desviación estándar");
+    Console.WriteLine("5. Mostrar números ingresados");
+    Console.WriteLine("6. Salir");
+
+    Console.Write("\nSeleccione una opción: ");
+    int opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            //PedirNumeros();
+            break;
+        case 2:
+            //CalcularYMostrarMedia();
+            break;
+        case 3:
+            //CalcularYMostrarMediana();
+            break;
+        case 4:
+            //CalcularYMostrarDesviacionEstandar();
+            break;
+        case 5:
+            //MostrarNumerosIngresados();
+            break;
+        case 6:
+            Console.WriteLine("¡Hasta luego!");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opción no válida. Por favor, seleccione una opción del menú.");
+            break;
+    }
+
+    MostrarMenu(); 
+}
